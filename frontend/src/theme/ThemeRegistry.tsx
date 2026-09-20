efc1a86fa602b65c864fd90834e50946fdfc6f59
rigobertomadrid <josue_madrid@unitec.edu>
2026-09-19 21:11:28 -0600

    Correccion de MUI con App Router

--- frontend/src/theme/ThemeRegistry.tsx
+++ frontend/src/theme/ThemeRegistry.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -11,9 +12,11 @@ interface ThemeRegistryProps {
 
 export default function ThemeRegistry({ children }: ThemeRegistryProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
+    <AppRouterCacheProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </AppRouterCacheProvider>
   );
 }
